418721058d4584dbd8cc8c507849814e0c75f6c2
SieunSong <00sieun0608@naver.com>
2018-09-15 23:55:54 +0900

    source code update

--- Rest/app/src/main/java/com/sesong/rest/MainActivity.java
+++ Rest/app/src/main/java/com/sesong/rest/MainActivity.java
@@ -1,8 +1,18 @@
 package com.sesong.rest;
 
-import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,14 +20,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
     TextView mainText;
     Button eyeStretch;
     ImageView rightSeat, phoneNoti, blueScreen;
+    private Handler handler = new Handler();
+    int click = 0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -26,23 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
         final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         mainText = (TextView) findViewById(R.id.maintext);
         rightSeat = (ImageView) findViewById(R.id.rightseat);
         phoneNoti = (ImageView) findViewById(R.id.noti);
         eyeStretch = (Button) findViewById(R.id.eyesBtn);
         blueScreen = (ImageView) findViewById(R.id.bluescreen);
-
-        final ArrayList<String> stretching = new ArrayList<>();
-        stretching.add("초점을 맞추지 않은 채로 가볍게 위를 본다");
-        stretching.add("눈을 감는다");
-        stretching.add("눈을 최대한 부릅뜬다");
-        stretching.add("양쪽 시선을 우측으로 고정한다");
-        stretching.add("양쪽 시선을 좌측으로 고정한다");
-        stretching.add("양쪽 시선을 위쪽으로 고정한다");
-        stretching.add("양쪽 시선을 아래쪽으로 고정한다");
-        stretching.add("눈 주위를 누르며\n\n안쪽에서 바깥쪽으로 문질러준다");
-        stretching.add("눈꺼풀 위를 가볍게 누른다");
-        mainText.setTextSize(12);
 
         rightSeat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,31 +56,40 @@ public class MainActivity extends AppCompatActivity {
         phoneNoti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                phoneNoti.setColorFilter(R.color.Salmon);
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
-                builder.setContentTitle("스마트폰을 사용한 지 " + i + "시간이 지났습니다.")
-                        .setContentText("장시간 스마트폰 사용은 눈 건강에 해로울 수 있습니다.")
-                .setDefaults(Notification.DEFAULT_SOUND)
-                .setLargeIcon(R.drawable.mainicon)
-                .setAutoCancel(true);
-
+                setTimeNoti(v);
             }
         });
         eyeStretch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CountDownTimer timer = new CountDownTimer(27000, 3000) {
+                click = 1;
+                final ArrayList<String> stretching = new ArrayList<>();
+                stretching.add("초점을 맞추지 않은 채로 가볍게 위를 본다");
+                stretching.add("눈을 감는다");
+                stretching.add("눈을 최대한 부릅뜬다");
+                stretching.add("양쪽 시선을 우측으로 고정한다");
+                stretching.add("양쪽 시선을 좌측으로 고정한다");
+                stretching.add("양쪽 시선을 위쪽으로 고정한다");
+                stretching.add("양쪽 시선을 아래쪽으로 고정한다");
+                stretching.add("눈 주위를 누르며\n\n안쪽에서 바깥쪽으로 문질러준다");
+                stretching.add("눈꺼풀 위를 가볍게 누른다");
+                stretching.add(":)");
+                mainText.setTextSize(12);
+                CountDownTimer timer = new CountDownTimer(33000, 3000) {
                     int i = 0;
+
                     public void onTick(long millisUntilFinished) {
                         mainText.setText(stretching.get(i));
-                        vibrator.vibrate(500);
+                        vibrator.vibrate(200);
                         i++;
                     }
+
                     public void onFinish() {
+                        click = 0;
+                        // timer.cancel();
                         mainText.setTextSize(100);
                     }
                 }.start();
-                timer.cancel();
             }
         });
         blueScreen.setOnClickListener(new View.OnClickListener() {
@@ -86,5 +98,66 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void setTimeNoti(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 60; i++) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    final int time = i;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (click == 0) {
+                                // UI 갱신
+                                mainText.setText(String.valueOf(time));
+                                progressBar.setProgress(time);
+                            } else {
+                                progressBar.setProgress(time);
+                            }
+                            if (time ==60) show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+    private void show() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+        // 필수 항목
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("스마트폰 사용시간 경고");
+        builder.setContentText("과도한 스마트폰의 사용은 눈 건강에 해로울 수 있습니다.");
+        // 액션 정의
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // 클릭 이벤트 설정
+        builder.setContentIntent(pendingIntent);
+        // 큰 아이콘 설정
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.mainicon);
+        builder.setLargeIcon(largeIcon);
+        // 색상 변경
+        builder.setColor(Color.RED);
+        // 기본 알림음 사운드 설정
+        Uri ringtoneUri = RingtoneManager.getActualDefaultRingtoneUri(this, RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(ringtoneUri);
+        // 진동 설정: 대기시간, 진동시간, 대기시간, 진동시간 ... 반복패턴
+        long[] vibrate = {0, 100, 200, 300};
+        builder.setVibrate(vibrate);
+        builder.setAutoCancel(true);
+        // 알림 매니저
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // 오레오에서는 알림 채널을 매니저에 생성해야 한다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        // 알림 통지
+        manager.notify(1, builder.build());
     }
 }
